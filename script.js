ac4947881cfffb6c79e0a5414695d4819e8e07fe
pineda.juan92 <pineda.juan92@gmail.com>
2021-08-28 13:19:39 -0500

    Javascript enhancements using async - await

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const REPO_API_URL = "https://api.github.com/users/juan1018pineda/repos";
 
-function createElementWithHtml(tag, innerHTML){
-    const element = document.createElement(tag);
-    element.innerHTML = innerHTML;
-    return element;
+function createElementWithHtml(tag, innerHTML) {
+  const element = document.createElement(tag);
+  element.innerHTML = innerHTML;
+  return element;
 }
 
-function appendChilds(container, childs){
-    childs.forEach(child => container.appendChild(child));
+function appendChilds(container, childs) {
+  childs.forEach((child) => container.appendChild(child));
 }
 
-function setHtmlTag(element, tag, value){
+function setHtmlTag(element, tag, value) {
   return element.setAttribute(tag, value);
 }
 
@@ -18,27 +18,28 @@ function showRepos(repos) {
   let render = document.getElementById("repos");
   let repoList = document.createElement("ul");
   repos.forEach((repo) => {
-    const repoLink = createElementWithHtml('a', 'Ver repositorio');
-    setHtmlTag(repoLink,'href',repo.html_url);
-    setHtmlTag(repoLink,'target','_blank');
-    const repoItem = createElementWithHtml('li', `<span><b>Project Name:</b> ${repo.name}</span>`);
-    const repoOwner = createElementWithHtml('p', `<b>Author:</b> ${repo.owner.login}`);
+    const repoLink = createElementWithHtml("a", "Ver repositorio");
+    const repoItem = createElementWithHtml(
+      "li",
+      `<span><b>Project Name:</b> ${repo.name}</span>`
+    );
+    const repoOwner = createElementWithHtml(
+      "p",
+      `<b>Author:</b> ${repo.owner.login}`
+    );
     const RepoImg = document.createElement("img");
-    setHtmlTag(RepoImg,'src',repo.owner.avatar_url);
+    setHtmlTag(repoLink, "href", repo.html_url);
+    setHtmlTag(repoLink, "target", "_blank");
+    setHtmlTag(RepoImg, "src", repo.owner.avatar_url);
     appendChilds(repoItem, [repoOwner, RepoImg, repoLink]);
     repoList.appendChild(repoItem);
   });
   render.appendChild(repoList);
 }
 
-function getRepos() {
-  fetch(REPO_API_URL)
-    .then((result) => {
-      return result.json();
-    })
-    .then((repos) => {
-      showRepos(repos);
-    });
+async function getRepos() {
+  const response = await fetch(REPO_API_URL);
+  showRepos(await response.json());
 }
 
 getRepos();
