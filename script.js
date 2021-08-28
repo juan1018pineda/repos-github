cf4a1b738c919f89e0234379acd46b4fd3895226
pineda.juan92 <pineda.juan92@gmail.com>
2021-08-28 13:06:34 -0500

    Code enhancements for HTML, CSS and JS

--- script.js
+++ script.js
@@ -10,17 +10,21 @@ function appendChilds(container, childs){
     childs.forEach(child => container.appendChild(child));
 }
 
+function setHtmlTag(element, tag, value){
+  return element.setAttribute(tag, value);
+}
+
 function showRepos(repos) {
   let render = document.getElementById("repos");
   let repoList = document.createElement("ul");
   repos.forEach((repo) => {
     const repoLink = createElementWithHtml('a', 'Ver repositorio');
-    repoLink.setAttribute('href', repo.html_url);
-    repoLink.setAttribute('target', '_blank');
-    const repoItem = createElementWithHtml('li', repo.name);
-    const repoOwner = createElementWithHtml('p', repo.owner.login);
+    setHtmlTag(repoLink,'href',repo.html_url);
+    setHtmlTag(repoLink,'target','_blank');
+    const repoItem = createElementWithHtml('li', `<span><b>Project Name:</b> ${repo.name}</span>`);
+    const repoOwner = createElementWithHtml('p', `<b>Author:</b> ${repo.owner.login}`);
     const RepoImg = document.createElement("img");
-    RepoImg.setAttribute('src',repo.owner.avatar_url);
+    setHtmlTag(RepoImg,'src',repo.owner.avatar_url);
     appendChilds(repoItem, [repoOwner, RepoImg, repoLink]);
     repoList.appendChild(repoItem);
   });
